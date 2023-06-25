64ec03d3cc9098bebdc7da8f3e63fdfe36c90157
Kevin Wu <kevinwu098@gmail.com>
2023-06-25 00:28:10 -0700

    Added new tab for image uploading, alongside some style tweaks

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 import gCalendar, weather, chatAI
 
-# event_summaries = ["Pickleball match", "Business meeting with executives"]
+# Test events
+# event_summaries = ["Dinner Date"]
 # event_summaries = ', '.join(event_summaries)
-
-# event_summaries = gCalendar.getCalendarEvents()
-# print(event_summaries)
-# if isinstance(event_summaries, list):
-#     event_summaries = ", ".join(event_summaries)
-
-# suggested_outfit = chatAI.CustomChatGPT(event_summaries)
-# print(suggested_outfit)
 
 import gradio as gr
 
@@ -21,7 +14,7 @@ def getOutfit(dummy_argument):
     generated_outfit = chatAI.CustomChatGPT(event_summaries)
 
     return (
-        "Your Events: \n"
+        "Your Event(s): \n"
         f'{event_summaries} \n'
 
         "\n"
@@ -30,32 +23,81 @@ def getOutfit(dummy_argument):
         f'{generated_outfit}'
     )
 
+import requests
+API_KEY = "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"
+API_URL = "https://api-inference.huggingface.co/models/microsoft/resnet-50"
+headers = {"Authorization": f"Bearer {API_KEY}"}
+
+def classifyClothing(filepath):
+    def query(filename):
+        with open(filename, "rb") as f:
+            data = f.read()
+        response = requests.post(API_URL, headers=headers, data=data)
+        return response.json()
+
+    output = query(filepath)
+
+    return output[0]["label"] if output else None
+
 with gr.Blocks() as demo:
-    with gr.Row():
+    with gr.Tab("Get Outfit(s) Based on Your Schedule"):
+        with gr.Row():
+            gr.Markdown(
+                """
+                <h1> <center> IntelliStyle </center> </h1>
+                <h3> <center> 
+                IntelliStyle is integrated with your Google Calendar, allowing it to suggest optimized outfits for your upcoming day. 
+                </br>
+                It also takes into account weather conditions, making sure you're looking your best — rain or shine. 
+                </center> </h3>
+                """
+            )
+
+        with gr.Row():
+            outputs=gr.Textbox(lines=5, label="Generated Outfit")
+        
+        with gr.Row():
+            btn = gr.Button("Run")
+            btn.click(fn=getOutfit, inputs=btn, outputs=outputs)
+
+        with gr.Row():
+            gr.Markdown(
+                """
+                </br>
+                <h4 style="text-align:center"> <i><u> Please do not spam the button as errors will occur if you use it too rapidly. </u></i> </h4>
+                """
+        )
+    
+    with gr.Tab("Upload Clothing Items"):
         gr.Markdown(
             """
-            <h1> <center> IntelliStyle </center> </h1>
+            <h1> <center> Upload Clothes to IntelliStyle </center> </h1>
             <h3> <center> 
-            IntelliStyle is integrated with your Google Calendar, allowing it to suggest optimized outfits for your upcoming day. 
+            Adding images of your clothing allows Intellistyle to make even better recommendations — tailored just for you. 
             </br>
-            It also takes into account weather conditions, making sure you're looking your best — rain or shine. 
+            Clothing items are best uploaded on their own, preferably on a clean backdrop while <b>not</b> being worn.
             </center> </h3>
             """
         )
 
-    with gr.Row():
-        outputs=gr.Textbox(lines=5, label="Generated Outfit")
-    
-    with gr.Row():
+        with gr.Row():
+            inp = gr.Image(type="filepath")
+            out = gr.Textbox(label="What article of clothing we think you uploaded")
+
         btn = gr.Button("Run")
-        btn.click(fn=getOutfit, inputs=btn, outputs=outputs)
+        btn.click(fn=classifyClothing, inputs=inp, outputs=out)
 
-    with gr.Row():
-        gr.Markdown(
-            """
-            </br>
-            <h4 style="text-align:center"> <i><u> Please do not spam the button as errors will occur if you use it too rapidly. </u></i> </h4>
-            """
+        with gr.Row():
+            gr.Markdown("""<h1><center>Examples ↓</center><h1>""")
+        
+        import os
+        gr.Examples(
+            examples=[
+                [os.path.join(os.path.dirname(__file__), "example_tshirt.jpg")],
+            ],            
+            inputs = gr.Image(type="filepath", height=400),
+            outputs = gr.Textbox(label="What article of clothing IntelliStyle thinks was uploaded"),
+            fn=classifyClothing,
+            cache_examples=True,
         )
-
 demo.launch()
--- weather.py
+++ weather.py
@@ -1,7 +1,13 @@
 import requests
+import datetime
 
 def getWeather():
-    url = "https://api.open-meteo.com/v1/forecast?latitude=34.05&longitude=-118.24&hourly=temperature_2m,relativehumidity_2m,rain&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&forecast_days=1&start_date=2023-06-24&end_date=2023-06-24&timezone=America%2FLos_Angeles"
+
+# Get the current date
+    current_date = datetime.date.today().isoformat()
+
+    # Replace the start_date and end_date in the URL
+    url = "https://api.open-meteo.com/v1/forecast?latitude=34.05&longitude=-118.24&hourly=temperature_2m,relativehumidity_2m,rain&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&forecast_days=1&start_date={}&end_date={}&timezone=America%2FLos_Angeles".format(current_date, current_date)
     response = requests.get(url)
     data = response.json()
 
